1085cc52f2f94bc6ed425c1cef41904ed4193d95
staujd02 <staujd02@students.ipfw.edu>
2018-10-11 10:41:01 -0400

    queue: green tests

--- queue.py
+++ queue.py
@@ -1,6 +1,6 @@
 class PriorityQueue(object): 
     def __init__(self): 
-        self.queue = [] 
+        self.queue = {} 
   
     def __str__(self): 
         return ' '.join([str(i) for i in self.queue]) 
@@ -9,16 +9,20 @@ class PriorityQueue(object):
         return len(self.queue) == 0 
   
     def insert(self, data, priority): 
-        self.queue.append(data) 
+        self.queue[str(priority)] = [data]
   
     def pop(self): 
-        try: 
-            max = 0
-            for i in range(len(self.queue)): 
-                if self.queue[i] > self.queue[max]: 
-                    max = i 
-            item = self.queue[max] 
-            del self.queue[max] 
+        try:
+            max = -1
+            for x in self.queue:
+                if(max == -1):
+                    max = x
+                    continue
+                if int(x) < int(max): 
+                    max = x
+            item = self.queue[max].pop()
+            if len(self.queue[max]) == 0:
+                del self.queue[max] 
             return item 
         except IndexError: 
             print() 
